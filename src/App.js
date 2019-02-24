f00269c1adea2b9001afa0c09c86f92cec10d789
johnromanoff <johnromanoff@yandex.ru>
2019-02-25 02:46:08 +0300

    create components

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-const App = () => (
-  <header>Blog</header>
-);
-
-export default App;
---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <React.Fragment>
+    <header>Blog</header>
+    <main>Статьи</main>
+  </React.Fragment>
+);
+
+export default App;
